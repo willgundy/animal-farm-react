fe2c07292f955a3c25521169dd865afba25bf7df
Will Gundy <willgunderson503@gmail.com>
2022-05-06 15:52:02 -0700

    added footer, main, and data files

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const animals = [
+  {
+    name: 'Betsy the Cow',
+    type: 'cow',
+    says: 'Mooooooo!',
+    top: '35%',
+    left: '50%',
+  },
+  {
+    name: 'Noel the Donkey',
+    type: 'donkey',
+    says: 'Heee haw!',
+    top: '55%',
+    left: '38%',
+  },
+  {
+    name: 'Golly the Goose',
+    type: 'goose',
+    says: 'Run pig! Be free! I would if I could.',
+    top: '60%',
+    left: '65%',
+  },
+  {
+    name: 'Wilbur the Pig',
+    type: 'pig',
+    says: 'You have been my friend. That in itself is a tremendous thing.',
+    top: '60%',
+    left: '5%',
+  },
+  {
+    name: 'Charlotte the Spider',
+    type: 'spider',
+    says: 'Never hurry and never worry!',
+    top: '42%',
+    left: '72%',
+  },
+  {
+    name: 'Samuel the Sheep',
+    type: 'sheep',
+    says: "Just because we're sheep, it doesn't mean we have to follow. Think for yourselves.",
+    top: '40%',
+    left: '20%',
+  },
+];
